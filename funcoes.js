24c01baf27e8ce12992a2ce1e7d4510db58346c0
Ana Losnak <ana.losnak@caelum.com.br>
2018-09-05 11:02:38 -0300

    Função formataPreco()

--- funcoes.js
+++ funcoes.js
@@ -2,3 +2,7 @@
 function pegaNumeroDoUsuario(texto) {
     return parseFloat(prompt(texto).replace(",","."));
 }
+
+function formataPreco(preco) {
+    return "R$" + preco.toFixed(2).replace(".",",");
+}
